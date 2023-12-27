f7c90e31b156705e521a8575dab1a420bab6e63d
crileyy <criley0007@gmail.com>
2023-12-26 22:40:44 -0800

    Update to old school font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import Navigation from '@/components/navigation'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Old_Standard_TT } from 'next/font/google'
 import Footer from '@/components/footer'
 import { cn } from "@/lib/utils"
 
-const inter = Inter({ subsets: ['latin'], variable: "--font-sans", })
+// const inter = Inter({ subsets: ['latin'], variable: "--font-sans", })
+const inter = Old_Standard_TT({ weight: '700', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Sinclair\'s Photography Portfolio',
@@ -22,10 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
+        // className={cn(
+        //   "min-h-screen bg-background font-sans antialiased",
+        //   inter.variable
+        // )}
+        className={inter.className}
       >
         <Navigation />
         {children}
